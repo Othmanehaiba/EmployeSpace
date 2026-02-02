32cb7bb6e74e0b547d7f20858d4d36dc1e03862f
Othmanehaiba <othmanehaiba4@gmail.com>
2026-02-02 17:30:34 +0100

    end of the day

--- database/migrations/2026_02_02_091227_create_candidate_table.php
+++ database/migrations/2026_02_02_091227_create_candidate_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('candidate', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id');
+
+            $table->string('speciallity');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,35 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $recruteur = User::factory()->create([
+            'name' => 'othmane',
+            'email'=> 'yons@gmail.com',
+            'photo'=> 'lkjvbsvbkfbvhqbdk',
+            'bio' => 'bzkhfqbhkjhfb',
+            'speciallity'=> 'ergjkfvhdbns'
+            
         ]);
+
+        $recruteur->assignRole('recruteur');
+
+        $chercheur = User::factory()->create([
+            'name' => 'yons',
+            'email'=> 'abid@gmail.com',
+            'photo'=> 'lkjvbsvbkfbvhqbdk',
+            'bio' => 'bzkhfqbhkjhfb',
+            'speciallity'=> 'ergjkfvhdbns'
+            
+        ]);
+
+        $chercheur->assignRole('chercheur'); 
+
+
+        User::factory(10)->create()->each(function ($user){
+            $user->assignRole('chercheur');
+        });
     }
 }
